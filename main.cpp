71a29b0f52cbcb6aad46ffa030637121132e5be7
Fredrik Olausson <fredrik@bluppfisk.org>
2017-02-20 19:25:09 +0100

    fixed order of command line parsing so MainWindow was not created if --version was used

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QCommandLineParser>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
 
-    w.show();
+	QCommandLineParser *cp = new QCommandLineParser();
+	cp->addHelpOption();
+	cp->addVersionOption();
+	a.setApplicationName("qtTemp");
+	a.setApplicationVersion(APP_VERSION);
+
+	cp->process(a);
+
+	delete cp;
+
+	MainWindow w;
+	w.show();
 
     int ret = a.exec();
 
